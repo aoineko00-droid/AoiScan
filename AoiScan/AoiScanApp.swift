13f8695badf3418d8cef4a2902c510ab761a554f
Aoineko <aoineko00@gmail.com>
2026-08-07 21:24:22 +0800

    Initial Commit

---
+++ AoiScan/AoiScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AoiScanApp.swift
+//  AoiScan
+//
+//  Created by Aoineko on 2026/8/7.
+//
+
+import SwiftUI
+
+@main
+struct AoiScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
